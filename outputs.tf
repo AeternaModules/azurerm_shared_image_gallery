9cfe4b690c983484fbd99c50b8926955eca2c6d3
Daniel Allen <dan@aeternagroup.net>
2026-01-11 18:09:02 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "shared_image_gallerys" {
+  description = "All shared_image_gallery resources"
+  value       = azurerm_shared_image_gallery.shared_image_gallerys
+}
+
---
+++ variables.tf
@@ -0,0 +1,37 @@
+variable "shared_image_gallerys" {
+  description = <<EOT
+Map of shared_image_gallerys, attributes below
+Required:
+    - location
+    - name
+    - resource_group_name
+Optional:
+    - description
+    - tags
+    - sharing (block):
+        - community_gallery (optional, block):
+            - eula (required)
+            - prefix (required)
+            - publisher_email (required)
+            - publisher_uri (required)
+        - permission (required)
+EOT
+
+  type = map(object({
+    location            = string
+    name                = string
+    resource_group_name = string
+    description         = optional(string)
+    tags                = optional(map(string))
+    sharing = optional(object({
+      community_gallery = optional(object({
+        eula            = string
+        prefix          = string
+        publisher_email = string
+        publisher_uri   = string
+      }))
+      permission = string
+    }))
+  }))
+}
+
