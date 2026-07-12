9a27409cfc9c47c8bac74d023afce7f34cb08731
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:20:23 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,33 +1,29 @@
-output "shared_image_galleries" {
-  description = "All shared_image_gallery resources"
-  value       = azurerm_shared_image_gallery.shared_image_galleries
-}
 output "shared_image_galleries_description" {
-  description = "List of description values across all shared_image_galleries"
-  value       = [for k, v in azurerm_shared_image_gallery.shared_image_galleries : v.description]
+  description = "Map of description values across all shared_image_galleries, keyed the same as var.shared_image_galleries"
+  value       = { for k, v in azurerm_shared_image_gallery.shared_image_galleries : k => v.description }
 }
 output "shared_image_galleries_location" {
-  description = "List of location values across all shared_image_galleries"
-  value       = [for k, v in azurerm_shared_image_gallery.shared_image_galleries : v.location]
+  description = "Map of location values across all shared_image_galleries, keyed the same as var.shared_image_galleries"
+  value       = { for k, v in azurerm_shared_image_gallery.shared_image_galleries : k => v.location }
 }
 output "shared_image_galleries_name" {
-  description = "List of name values across all shared_image_galleries"
-  value       = [for k, v in azurerm_shared_image_gallery.shared_image_galleries : v.name]
+  description = "Map of name values across all shared_image_galleries, keyed the same as var.shared_image_galleries"
+  value       = { for k, v in azurerm_shared_image_gallery.shared_image_galleries : k => v.name }
 }
 output "shared_image_galleries_resource_group_name" {
-  description = "List of resource_group_name values across all shared_image_galleries"
-  value       = [for k, v in azurerm_shared_image_gallery.shared_image_galleries : v.resource_group_name]
+  description = "Map of resource_group_name values across all shared_image_galleries, keyed the same as var.shared_image_galleries"
+  value       = { for k, v in azurerm_shared_image_gallery.shared_image_galleries : k => v.resource_group_name }
 }
 output "shared_image_galleries_sharing" {
-  description = "List of sharing values across all shared_image_galleries"
-  value       = [for k, v in azurerm_shared_image_gallery.shared_image_galleries : v.sharing]
+  description = "Map of sharing values across all shared_image_galleries, keyed the same as var.shared_image_galleries"
+  value       = { for k, v in azurerm_shared_image_gallery.shared_image_galleries : k => v.sharing }
 }
 output "shared_image_galleries_tags" {
-  description = "List of tags values across all shared_image_galleries"
-  value       = [for k, v in azurerm_shared_image_gallery.shared_image_galleries : v.tags]
+  description = "Map of tags values across all shared_image_galleries, keyed the same as var.shared_image_galleries"
+  value       = { for k, v in azurerm_shared_image_gallery.shared_image_galleries : k => v.tags }
 }
 output "shared_image_galleries_unique_name" {
-  description = "List of unique_name values across all shared_image_galleries"
-  value       = [for k, v in azurerm_shared_image_gallery.shared_image_galleries : v.unique_name]
+  description = "Map of unique_name values across all shared_image_galleries, keyed the same as var.shared_image_galleries"
+  value       = { for k, v in azurerm_shared_image_gallery.shared_image_galleries : k => v.unique_name }
 }
 
